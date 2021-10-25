9b2984677138bc54c78c70ab88b99a4518239538
JamesRobert20 <jmtendamema@gmail.com>
2021-10-25 09:10:09 -0600

    daynamer class editing to fit scheduler

--- Java Files/Scheduler/src/daynamer.java
+++ Java Files/Scheduler/src/daynamer.java
@@ -1,5 +1,11 @@
 import java.util.*;
 public class daynamer {
+	private String dategiven;
+	
+	public daynamer(String theDate)
+	{
+		dategiven = theDate;
+	}
 	
 	public static String dayName(int counter)
 	{
@@ -24,69 +30,69 @@ public class daynamer {
 		return day;
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//Initializing the input object 
-				Scanner input = new Scanner(System.in);
-				System.out.println("Enter the date of the day(dd/mm/yyyy): ");
-				
-				//Initializing user input to a variable
-				String the_date_string = input.nextLine().strip();
-				input.close();
-				
-				InputChecker e = new InputChecker(the_date_string);
-				
-				if(e.inputIsCorrect())
-				{
-					int day_counter = 0;
-					
-					for(int i = 1; i < e.getYear(); i++)
-					{
-						if(i % 4 == 0)
-						{
-							day_counter = day_counter + 366;
-						}
-						else
-						{
-							day_counter = day_counter + 365;
-						}
-					}
-			
-					for(int j = 1; j < e.getMonth(); j++)
-					{
-						
-						if(!e.getSmall_months().contains(j))
-						{
-							day_counter = day_counter + 31;
-						}
-						else
-						{
-							if(e.getMonths().get(j) == "February")
-							{
-									if(e.getYear() % 4 == 0)
-										day_counter = day_counter + 29; 
-									else
-									{
-										day_counter = day_counter + 28;
-									}
-							}
-							else
-							{
-								day_counter = day_counter + 30;
-							}
-						}
-					}
-					
-					day_counter = day_counter + e.getDate();
-					String the_day = dayName(day_counter);
-					System.out.println(the_day);
-				}
-				else
-				{
-					return;
-				}
-
+	public String getDategiven() {
+		return dategiven;
 	}
+	
+	
+	public  String determineDay() {
+		String output = "";
+			
+			InputChecker e = new InputChecker(getDategiven());
+				
+			if(e.inputIsCorrect())
+			{
+				int day_counter = 0;
+					
+				for(int i = 1; i < e.getYear(); i++)
+				{
+					if(i % 4 == 0)
+					{
+						day_counter = day_counter + 366;
+					}
+					else
+					{
+						day_counter = day_counter + 365;
+					}
+				}
+			
+				for(int j = 1; j < e.getMonth(); j++)
+				{
+						
+					if(!e.getSmall_months().contains(j))
+					{
+						day_counter = day_counter + 31;
+					}
+					else
+					{
+						if(e.getMonths().get(j) == "February")
+						{
+								if(e.getYear() % 4 == 0)
+									day_counter = day_counter + 29; 
+								else
+								{
+									day_counter = day_counter + 28;
+								}
+						}
+						else
+						{
+							day_counter = day_counter + 30;
+						}
+					}
+				}
+					
+				day_counter = day_counter + e.getDate();
+				String the_day = dayName(day_counter);
+				System.out.println(the_day);
+			}
+			else
+			{
+				output = "Wrong input format! should be dd/mm/yyyy";
+			}
+			return output;
+			
+	}
+
+	
 
 }
--- Java Files/Scheduler/src/scheduler.java
+++ Java Files/Scheduler/src/scheduler.java
@@ -1,4 +1,10 @@
 
 public class scheduler {
-
+	
+	public static void main(String[] args)
+	{
+		
+		daynamer e = new daynamer("");
+		System.out.println(e.determineDay());
+	}
 }
